0ff0a7b22d53d5dcdd040452928d9c9cfcb61541
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-05 01:04:24 +0600

    genarate a token and send as browser cookie while successfully loggedin

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import User, { IUser } from "../models/user.model";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const saltRounds = 10;
 
@@ -37,13 +38,36 @@ export const registerUser = async (req: Request & { body: IUser }, res: Response
 export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { email, password } = req.body;
+
+        // find the user by email and if not found then return a message
         const user = await User.findOne({ email: email });
         if (!user) {
             return res.status(404).send({ success: false, error: "User not found!" })
         }
+
+        // compare the hashing password
         bcrypt.compare(password, user.password, (err, result) => {
+            // if password matched then successfully logged in otherwise send a message
             if (result) {
-                return res.status(200).send({ success: true, message: "Login Successful!", user });
+
+                // genarate a token
+                const tokenSecret = process.env.JWT_TOKEN;
+                if (!tokenSecret) throw new Error("JWT_TOKEN is missing in env file");
+                const userData = { email: user.email, id: user._id };
+                const token = jwt.sign(userData, tokenSecret, { expiresIn: "30d" })
+
+                // cookie options
+                const cookieOptions: { httpOnly: boolean, sameSite: "strict" | "lax" | "none" | boolean, secure: boolean } = {
+                    httpOnly: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+                    secure: process.env.NODE_ENV === 'production',
+                };
+
+                // response send to the client
+                return res.status(200)
+                    .cookie("token", token, cookieOptions)
+                    .send({ success: true, message: "Login Successful!", user });
+                    
             }
             else {
                 return res.status(401).send({ success: false, message: "Wrong Password!" });
