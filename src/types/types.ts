ccb6c6ce25b6dc009e9f1d4fb9b884acd7ffb498
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-13 23:58:09 +0600

    define type of shop details

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,22 @@
+import mongoose from "mongoose";
+
 export interface ICookieOptions {
     httpOnly: boolean;
     sameSite: "strict" | "lax" | "none" | boolean;
     secure: boolean;
     maxAge?: number;
     expires?: Date;
+}
+
+export interface IShop {
+    _id: mongoose.Types.ObjectId;
+    name: string;
+    manager: string;
+    barbers: mongoose.Types.ObjectId[];
+    images: string[];
+    license: string;
+    ratings: mongoose.Types.ObjectId[];
+    location: object;
+    services: string[];
+    bookings: mongoose.Types.ObjectId[];
 }
