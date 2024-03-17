54baafdd61fc6e2b1da2b077155ea1c710d32f33
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-17 23:20:43 +0600

    fix: createShop controller and include new owner create feature

--- src/controllers/owner.controllers.ts
+++ src/controllers/owner.controllers.ts
@@ -5,9 +5,20 @@ import Owner from "../models/owner.model";
 // controller for create a new store
 export const createShop = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { name, owner, barbers, shopImages, license, location, services } = req.body;
-        const newShop = new Shop({ name, owner, barbers, shopImages, license, location, services });
+        // get all data from req.body
+        const { name, owner, barbers, shopImages, license, location, services, experience, specialties } = req.body;
+
+        // set all data
+        const newShop = new Shop({ name, barbers, shopImages, license, location, services, });
+        const newOwner = new Owner({ user: owner, experience, specialties });
+
+        // set shop and owner id
+        newShop.owner = newOwner._id;
+        newOwner.shop = newShop._id;
+
+        // save newShop and newOwner
         await newShop.save();
+        await newOwner.save();
         res.status(201).send({ success: true, message: "Store created successfully" })
     } catch (error) {
         next(error);
