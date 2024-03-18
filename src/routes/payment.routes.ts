a7cbf93a254bc0bbe7ef0e1e0a95e2c0b7d41fab
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-03-18 19:23:25 +0600

    implement post endpoint to create a new payment

--- src/routes/payment.routes.ts
+++ src/routes/payment.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { createPayment } from "../controllers/payment.controllers";
 
 const router = Router();
 
-
+router.post('/create-payment', createPayment)
 
 export default router;
