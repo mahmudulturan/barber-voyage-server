b74fd11243ea3d894f6683be8b06b84c81d785d3
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-07-22 20:03:59 +0600

    chore: change file name of errorHandler

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
-import errorHandler from './app/middlewares/errorHandler';
 import session from 'express-session';
 import configs from './app/configs';
 import routes from './app/routes';
@@ -11,6 +10,7 @@ import routes from './app/routes';
 // configs
 import './app/configs/database';
 import './app/configs/passport';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 // create app
 const app = express();
@@ -46,8 +46,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     res.status(404).send({ message: "The specified route cannot be located or identified." })
 })
 
-// Use the error handler middleware
-app.use(errorHandler);
+// handling all erorrs in one error handling middleware
+app.use(globalErrorHandler);
 
 
 export default app;
